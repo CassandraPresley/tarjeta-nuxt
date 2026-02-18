d4d9a5f0125a765a15890a87ef5cfd03e65f48c2
Cassandra Presley <isawoodspines@gmail.com>
2026-02-18 07:50:08 -0600

    Primer commit

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container">
+    <div class="card">
+      <!-- IMAGEN -->
+      <img src="/cass.jpg" class="cat" alt="Foto" />
+      <h1>ING. ROWEENA ISADORA<br />SÁNCHEZ FLORES</h1>
+      <h2>Ingeniería en Tecnologías de la Información y Comunicaciones</h2>
+
+      <div class="divider"></div>
+
+      <div class="skills">
+        💡 <strong>Habilidades:</strong><br />
+        Manualidades y creación de páginas web
+      </div>
+
+      <p class="info">📞 <strong>Teléfono:</strong> 5526747793</p>
+      <p class="info">📧 <strong>Correo:</strong><br />ISAWOODSPINES@GMAIL.COM</p>
+      <p class="info">💗 <strong>Gustos:</strong><br />AMO CAIFANES Y A ELVIS PRESLEY Y A LOS GATITOS ESPONJOSOS</p>
+
+      <div class="divider"></div>
+
+      <p class="footer">Ciudad de México, México 💜✨</p>
+    </div>
   </div>
 </template>
+
